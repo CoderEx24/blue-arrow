7afdb2d70c04106b03f9b9e3c2e1b6a71a1c7b4c
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-20 12:24:45 +0200

    added basic checks

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 echo "BlueArrow - Starting to prepare that bad boi"
 echo "Checking..."
+
+# checking for config
+# exit if it doesn't exists
+if [ ! -f './config' ]; then
+    echo "config not found, exiting"
+    exit 0
+fi
+
+# checking for program_list
+# exit if it doesn't exists
+if [ ! -f './program_list' ]; then
+    echo "program_list not found, exiting"
+    exit 0
+fi
+
+# include config
+source ./config
+
+# checking for install_cmd command
+# exit if not set
+if [ "$install_cmd" = "" ]; then
+    echo '$install_cmd is not set, exiting'
+    exit 0
+fi
+
+echo "Checks done, all good"
+
 
