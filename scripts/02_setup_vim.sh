7bea999980fac39dcb35bec437ec8030ce1c6919
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-22 09:08:48 +0200

    made a script to setup vim

---
+++ scripts/02_setup_vim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# clone the ultimate vim configuration
+git clone https://github.com/amix/vimrc ~/.vim_runtime
+
+# install it
+cd ~/.vim_runtime
+chmod +x ./install_awesome_vimrc.sh
+./install_awesome_vimrc.sh
+
+# clone YouCompleteMe
+git clone https://github.com/ycm-core/YouCompleteMe ./my_plugins/YouCompleteMe/
+
+# install it
+cd ./my_plugins/YouComleteMe
+./install.py --clangd-completer --java-completer
+
