33011c95f70f9195e9232fc013515e53a02ca49a
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-30 11:16:03 +0200

    added sxiv + some cleanup

--- scripts/01_setup_x.sh
+++ scripts/01_setup_x.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # download necessary stuff
-git clone 'https://github.com/CoderEx24/dwm' '/tmp/dwm'
-git clone 'https://github.com/CoderEx24/slstatus' '/tmp/slstatus'
+git clone https://github.com/CoderEx24/dwm /tmp/dwm
+git clone https://github.com/CoderEx24/slstatus /tmp/slstatus
+git clone https://github.com/muennich/sxiv /tmp/sxiv
 curl 'https://dl.suckless.org/tools/dmenu-5.0.tar.gz' --output '/tmp/dmenu.tar.gz'
 tar -xvf '/tmp/dmenu.tar.gz'
 
@@ -18,5 +19,9 @@ sudo make clean install
 cd '/tmp/dmenu-5.0'
 sudo make clean install
 
+# compile and install sxiv
+cd /tmp/sxiv
+sudo make clean install
+
 # cleanup - remove all unnecessary stuff
-rm -rf '/tmp/dwm' '/tmp/dmenu-5.0' '/tmp/slstatus' '/tmp/dmenu.tar.gz'
+rm -rf /tmp/dwm '/tmp/dmenu-5.0' /tmp/slstatus /tmp/sxiv '/tmp/dmenu.tar.gz'
