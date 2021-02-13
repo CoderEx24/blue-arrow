eea0a3ef80225e421581500d8b68e71bb21d2454
CoderEx24 <kareemhacker123321@gmail.com>
2021-02-13 06:47:20 +0200

    modified how the scripts sets up X

--- scripts/01_setup_x.sh
+++ scripts/01_setup_x.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+scripts_dir="`pwd`"
+
 # download necessary stuff
-git clone https://github.com/CoderEx24/dwm /tmp/dwm
-git clone https://github.com/CoderEx24/slstatus /tmp/slstatus
-git clone https://github.com/muennich/sxiv /tmp/sxiv
+curl https://dl.suckless.org/dwm/dwm-6.2.tar.gz --output /tmp/dwm.tar.gz
 curl 'https://dl.suckless.org/tools/dmenu-5.0.tar.gz' --output '/tmp/dmenu.tar.gz'
-tar -xvf '/tmp/dmenu.tar.gz'
+git clone https://git.suckless.org/slstatus /tmp/slstatus
+git clone https://github.com/muennich/sxiv /tmp/sxiv
+cd /tmp
+tar -xvf dmenu.tar.gz
+tar -xvf dwm.tar.gz
 
 # compile and install dwm
-cd /tmp/dwm
+cd dwm-6.2
+cp config.def.h config.h
+patch config.h $scripts_dir/etc/myconfig_dwm.diff
 sudo make clean install
 
 # compile and install slstatus
 cd /tmp/slstatus
+cp $scripts_dir/etc/slstatus_config.h config.h
 sudo make clean install
 
 # compile and install dmenu
@@ -24,4 +31,4 @@ cd /tmp/sxiv
 sudo make clean install
 
 # cleanup - remove all unnecessary stuff
-rm -rf /tmp/dwm '/tmp/dmenu-5.0' /tmp/slstatus /tmp/sxiv '/tmp/dmenu.tar.gz'
+rm -rf /tmp/dwm-6.2 '/tmp/dmenu-5.0' /tmp/slstatus /tmp/sxiv '/tmp/dmenu.tar.gz' /tmp/dwm.tar.gz
