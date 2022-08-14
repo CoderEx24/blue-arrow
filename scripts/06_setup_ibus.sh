76b4a79159c4b2338dd5a0311573326f106069cf
CoderEx24 <coderex24@protonmail.com>
2022-08-14 11:34:44 +0200

    closes #6

--- scripts/06_setup_ibus.sh
+++ scripts/06_setup_ibus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-sudo echo -e "GTK_IM_MODULE=ibus\nQT_IM_MODULE=ibus\nXMODIFIERS=@im=ibus" >> /etc/environment
+sudo sh "echo -e \"GTK_IM_MODULE=ibus\\nQT_IM_MODULE=ibus\\nXMODIFIERS=@im=ibus\"" >> /etc/environment
 
