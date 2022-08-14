b1863988445798672e6152401fcbbd0c99b660ff
CoderEx24 <coderex24@protonmail.com>
2022-08-14 12:02:21 +0200

    bumped versions of dwm and dmenu

--- scripts/01_setup_x.sh
+++ scripts/01_setup_x.sh
@@ -3,8 +3,8 @@
 scripts_dir="`pwd`"
 
 # download necessary stuff
-curl https://dl.suckless.org/dwm/dwm-6.2.tar.gz --output /tmp/dwm.tar.gz
-curl 'https://dl.suckless.org/tools/dmenu-5.0.tar.gz' --output '/tmp/dmenu.tar.gz'
+curl 'https://dl.suckless.org/dwm/dwm-6.3.tar.gz' --output /tmp/dwm.tar.gz
+curl 'https://dl.suckless.org/tools/dmenu-5.1.tar.gz' --output /tmp/dmenu.tar.gz
 git clone https://git.suckless.org/slstatus /tmp/slstatus
 git clone https://github.com/muennich/sxiv /tmp/sxiv
 cd /tmp
@@ -12,7 +12,7 @@ tar -xvf dmenu.tar.gz
 tar -xvf dwm.tar.gz
 
 # compile and install dwm
-cd dwm-6.2
+cd 'dwm-6.3'
 cp config.def.h config.h
 patch config.h $scripts_dir/etc/myconfig_dwm.diff
 sudo make clean install
@@ -23,7 +23,7 @@ cp $scripts_dir/etc/slstatus_config.h config.h
 sudo make clean install
 
 # compile and install dmenu
-cd '/tmp/dmenu-5.0'
+cd '/tmp/dmenu-5.1'
 sudo make clean install
 
 # compile and install sxiv
@@ -31,4 +31,4 @@ cd /tmp/sxiv
 sudo make clean install
 
 # cleanup - remove all unnecessary stuff
-rm -rf /tmp/dwm-6.2 '/tmp/dmenu-5.0' /tmp/slstatus /tmp/sxiv '/tmp/dmenu.tar.gz' /tmp/dwm.tar.gz
+rm -rf '/tmp/dwm-6.3' '/tmp/dmenu-5.1' /tmp/slstatus /tmp/sxiv /tmp/dmenu.tar.gz /tmp/dwm.tar.gz
