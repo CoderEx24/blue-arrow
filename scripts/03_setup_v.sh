3ea09d7ba8f2c7d1d5f062b30d754049528382d0
CoderEx24 <coderex24@protonmail.com>
2022-08-14 11:53:34 +0200

    closes #5

--- scripts/03_setup_v.sh
+++ scripts/03_setup_v.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
-sudo git clone https://github.com/vlang/v /opt/v
-sudo cd /opt/v
-sudo make
+# don't change this to a directory owned by a user
+# other than the user currently running this script
+V_DIR=~/.local/lib/v/
+
+git clone https://github.com/vlang/v $V_DIR
+cd $V_DIR
+make
 sudo ./v symlink
